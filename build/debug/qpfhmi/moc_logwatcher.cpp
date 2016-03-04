a1fb5c657d50b35ffc1725455cce6733e0c9111a
jgonza04 <jgonza04@463500fe-d6be-4184-9b20-47717e75ecac>
2016-03-04 10:35:42 +0000

    Latest build
    git-svn-id: https://euclid.esac.esa.int/svn/ESA/SOC/SOC-3-DEV/SOC-3-07-QLook/QPF/trunk@11967 463500fe-d6be-4184-9b20-47717e75ecac

--- build/debug/qpfhmi/moc_logwatcher.cpp
+++ build/debug/qpfhmi/moc_logwatcher.cpp
@@ -19,8 +19,8 @@
 
 QT_BEGIN_MOC_NAMESPACE
 struct qt_meta_stringdata_QPF__LogWatcher_t {
-    QByteArrayData data[4];
-    char stringdata[36];
+    QByteArrayData data[5];
+    char stringdata[47];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -30,12 +30,14 @@ struct qt_meta_stringdata_QPF__LogWatcher_t {
 static const qt_meta_stringdata_QPF__LogWatcher_t qt_meta_stringdata_QPF__LogWatcher = {
     {
 QT_MOC_LITERAL(0, 0, 15), // "QPF::LogWatcher"
-QT_MOC_LITERAL(1, 16, 13), // "updateLogView"
-QT_MOC_LITERAL(2, 30, 0), // ""
-QT_MOC_LITERAL(3, 31, 4) // "path"
+QT_MOC_LITERAL(1, 16, 10), // "logUpdated"
+QT_MOC_LITERAL(2, 27, 0), // ""
+QT_MOC_LITERAL(3, 28, 13), // "updateLogView"
+QT_MOC_LITERAL(4, 42, 4) // "path"
 
     },
-    "QPF::LogWatcher\0updateLogView\0\0path"
+    "QPF::LogWatcher\0logUpdated\0\0updateLogView\0"
+    "path"
 };
 #undef QT_MOC_LITERAL
 
@@ -45,18 +47,24 @@ static const uint qt_meta_data_QPF__LogWatcher[] = {
        7,       // revision
        0,       // classname
        0,    0, // classinfo
-       1,   14, // methods
+       2,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
        0,       // flags
-       0,       // signalCount
+       1,       // signalCount
+
+ // signals: name, argc, parameters, tag, flags
+       1,    0,   24,    2, 0x06 /* Public */,
 
  // slots: name, argc, parameters, tag, flags
-       1,    1,   19,    2, 0x08 /* Private */,
+       3,    1,   25,    2, 0x08 /* Private */,
+
+ // signals: parameters
+    QMetaType::Void,
 
  // slots: parameters
-    QMetaType::Void, QMetaType::QString,    3,
+    QMetaType::Void, QMetaType::QString,    4,
 
        0        // eod
 };
@@ -66,8 +74,18 @@ void QPF::LogWatcher::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int 
     if (_c == QMetaObject::InvokeMetaMethod) {
         LogWatcher *_t = static_cast<LogWatcher *>(_o);
         switch (_id) {
-        case 0: _t->updateLogView((*reinterpret_cast< const QString(*)>(_a[1]))); break;
+        case 0: _t->logUpdated(); break;
+        case 1: _t->updateLogView((*reinterpret_cast< const QString(*)>(_a[1]))); break;
         default: ;
+        }
+    } else if (_c == QMetaObject::IndexOfMethod) {
+        int *result = reinterpret_cast<int *>(_a[0]);
+        void **func = reinterpret_cast<void **>(_a[1]);
+        {
+            typedef void (LogWatcher::*_t)();
+            if (*reinterpret_cast<_t *>(func) == static_cast<_t>(&LogWatcher::logUpdated)) {
+                *result = 0;
+            }
         }
     }
 }
@@ -97,14 +115,20 @@ int QPF::LogWatcher::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 1)
+        if (_id < 2)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 1;
+        _id -= 2;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 1)
+        if (_id < 2)
             *reinterpret_cast<int*>(_a[0]) = -1;
-        _id -= 1;
+        _id -= 2;
     }
     return _id;
+}
+
+// SIGNAL 0
+void QPF::LogWatcher::logUpdated()
+{
+    QMetaObject::activate(this, &staticMetaObject, 0, Q_NULLPTR);
 }
 QT_END_MOC_NAMESPACE
--- build/debug/qpfhmi/moc_mainwindow.cpp
+++ build/debug/qpfhmi/moc_mainwindow.cpp
@@ -19,8 +19,8 @@
 
 QT_BEGIN_MOC_NAMESPACE
 struct qt_meta_stringdata_QPF__MainWindow_t {
-    QByteArrayData data[31];
-    char stringdata[431];
+    QByteArrayData data[32];
+    char stringdata[452];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -33,48 +33,49 @@ QT_MOC_LITERAL(0, 0, 15), // "QPF::MainWindow"
 QT_MOC_LITERAL(1, 16, 15), // "goToOperational"
 QT_MOC_LITERAL(2, 32, 0), // ""
 QT_MOC_LITERAL(3, 33, 13), // "commandSystem"
-QT_MOC_LITERAL(4, 47, 20), // "transitToOperational"
-QT_MOC_LITERAL(5, 68, 10), // "sendInData"
-QT_MOC_LITERAL(6, 79, 21), // "prepareSendMultInData"
-QT_MOC_LITERAL(7, 101, 22), // "selectInDataParamsFile"
-QT_MOC_LITERAL(8, 124, 25), // "prepareSendInDataFromFile"
-QT_MOC_LITERAL(9, 150, 14), // "sendMultInData"
-QT_MOC_LITERAL(10, 165, 18), // "sendInDataFromFile"
-QT_MOC_LITERAL(11, 184, 21), // "stopSendingMultInData"
-QT_MOC_LITERAL(12, 206, 17), // "handleFinishedHMI"
-QT_MOC_LITERAL(13, 224, 15), // "checkForTaskRes"
-QT_MOC_LITERAL(14, 240, 11), // "showTaskRes"
-QT_MOC_LITERAL(15, 252, 24), // "showTaskMonitContextMenu"
-QT_MOC_LITERAL(16, 277, 1), // "p"
-QT_MOC_LITERAL(17, 279, 11), // "showWorkDir"
-QT_MOC_LITERAL(18, 291, 15), // "displayTaskInfo"
-QT_MOC_LITERAL(19, 307, 18), // "dumpTaskInfoToTree"
-QT_MOC_LITERAL(20, 326, 8), // "taskName"
-QT_MOC_LITERAL(21, 335, 12), // "Json::Value&"
-QT_MOC_LITERAL(22, 348, 1), // "v"
-QT_MOC_LITERAL(23, 350, 12), // "QTreeWidget*"
-QT_MOC_LITERAL(24, 363, 1), // "t"
-QT_MOC_LITERAL(25, 365, 10), // "dumpToTree"
-QT_MOC_LITERAL(26, 376, 16), // "QTreeWidgetItem*"
-QT_MOC_LITERAL(27, 393, 12), // "setDebugInfo"
-QT_MOC_LITERAL(28, 406, 1), // "b"
-QT_MOC_LITERAL(29, 408, 20), // "sortTaskViewByColumn"
-QT_MOC_LITERAL(30, 429, 1) // "c"
+QT_MOC_LITERAL(4, 47, 20), // "processPendingEvents"
+QT_MOC_LITERAL(5, 68, 20), // "transitToOperational"
+QT_MOC_LITERAL(6, 89, 10), // "sendInData"
+QT_MOC_LITERAL(7, 100, 21), // "prepareSendMultInData"
+QT_MOC_LITERAL(8, 122, 22), // "selectInDataParamsFile"
+QT_MOC_LITERAL(9, 145, 25), // "prepareSendInDataFromFile"
+QT_MOC_LITERAL(10, 171, 14), // "sendMultInData"
+QT_MOC_LITERAL(11, 186, 18), // "sendInDataFromFile"
+QT_MOC_LITERAL(12, 205, 21), // "stopSendingMultInData"
+QT_MOC_LITERAL(13, 227, 17), // "handleFinishedHMI"
+QT_MOC_LITERAL(14, 245, 15), // "checkForTaskRes"
+QT_MOC_LITERAL(15, 261, 11), // "showTaskRes"
+QT_MOC_LITERAL(16, 273, 24), // "showTaskMonitContextMenu"
+QT_MOC_LITERAL(17, 298, 1), // "p"
+QT_MOC_LITERAL(18, 300, 11), // "showWorkDir"
+QT_MOC_LITERAL(19, 312, 15), // "displayTaskInfo"
+QT_MOC_LITERAL(20, 328, 18), // "dumpTaskInfoToTree"
+QT_MOC_LITERAL(21, 347, 8), // "taskName"
+QT_MOC_LITERAL(22, 356, 12), // "Json::Value&"
+QT_MOC_LITERAL(23, 369, 1), // "v"
+QT_MOC_LITERAL(24, 371, 12), // "QTreeWidget*"
+QT_MOC_LITERAL(25, 384, 1), // "t"
+QT_MOC_LITERAL(26, 386, 10), // "dumpToTree"
+QT_MOC_LITERAL(27, 397, 16), // "QTreeWidgetItem*"
+QT_MOC_LITERAL(28, 414, 12), // "setDebugInfo"
+QT_MOC_LITERAL(29, 427, 1), // "b"
+QT_MOC_LITERAL(30, 429, 20), // "sortTaskViewByColumn"
+QT_MOC_LITERAL(31, 450, 1) // "c"
 
     },
     "QPF::MainWindow\0goToOperational\0\0"
-    "commandSystem\0transitToOperational\0"
-    "sendInData\0prepareSendMultInData\0"
-    "selectInDataParamsFile\0prepareSendInDataFromFile\0"
-    "sendMultInData\0sendInDataFromFile\0"
-    "stopSendingMultInData\0handleFinishedHMI\0"
-    "checkForTaskRes\0showTaskRes\0"
-    "showTaskMonitContextMenu\0p\0showWorkDir\0"
-    "displayTaskInfo\0dumpTaskInfoToTree\0"
-    "taskName\0Json::Value&\0v\0QTreeWidget*\0"
-    "t\0dumpToTree\0QTreeWidgetItem*\0"
-    "setDebugInfo\0b\0sortTaskViewByColumn\0"
-    "c"
+    "commandSystem\0processPendingEvents\0"
+    "transitToOperational\0sendInData\0"
+    "prepareSendMultInData\0selectInDataParamsFile\0"
+    "prepareSendInDataFromFile\0sendMultInData\0"
+    "sendInDataFromFile\0stopSendingMultInData\0"
+    "handleFinishedHMI\0checkForTaskRes\0"
+    "showTaskRes\0showTaskMonitContextMenu\0"
+    "p\0showWorkDir\0displayTaskInfo\0"
+    "dumpTaskInfoToTree\0taskName\0Json::Value&\0"
+    "v\0QTreeWidget*\0t\0dumpToTree\0"
+    "QTreeWidgetItem*\0setDebugInfo\0b\0"
+    "sortTaskViewByColumn\0c"
 };
 #undef QT_MOC_LITERAL
 
@@ -84,7 +85,7 @@ static const uint qt_meta_data_QPF__MainWindow[] = {
        7,       // revision
        0,       // classname
        0,    0, // classinfo
-      20,   14, // methods
+      21,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -92,28 +93,29 @@ static const uint qt_meta_data_QPF__MainWindow[] = {
        1,       // signalCount
 
  // signals: name, argc, parameters, tag, flags
-       1,    0,  114,    2, 0x06 /* Public */,
+       1,    0,  119,    2, 0x06 /* Public */,
 
  // slots: name, argc, parameters, tag, flags
-       3,    0,  115,    2, 0x0a /* Public */,
-       4,    0,  116,    2, 0x09 /* Protected */,
-       5,    0,  117,    2, 0x09 /* Protected */,
-       6,    0,  118,    2, 0x09 /* Protected */,
-       7,    0,  119,    2, 0x09 /* Protected */,
-       8,    0,  120,    2, 0x09 /* Protected */,
-       9,    0,  121,    2, 0x09 /* Protected */,
-      10,    0,  122,    2, 0x09 /* Protected */,
-      11,    0,  123,    2, 0x09 /* Protected */,
-      12,    0,  124,    2, 0x09 /* Protected */,
-      13,    0,  125,    2, 0x09 /* Protected */,
-      14,    0,  126,    2, 0x09 /* Protected */,
-      15,    1,  127,    2, 0x09 /* Protected */,
-      17,    0,  130,    2, 0x09 /* Protected */,
-      18,    0,  131,    2, 0x09 /* Protected */,
-      19,    3,  132,    2, 0x09 /* Protected */,
-      25,    2,  139,    2, 0x09 /* Protected */,
-      27,    1,  144,    2, 0x09 /* Protected */,
-      29,    1,  147,    2, 0x09 /* Protected */,
+       3,    0,  120,    2, 0x0a /* Public */,
+       4,    0,  121,    2, 0x0a /* Public */,
+       5,    0,  122,    2, 0x09 /* Protected */,
+       6,    0,  123,    2, 0x09 /* Protected */,
+       7,    0,  124,    2, 0x09 /* Protected */,
+       8,    0,  125,    2, 0x09 /* Protected */,
+       9,    0,  126,    2, 0x09 /* Protected */,
+      10,    0,  127,    2, 0x09 /* Protected */,
+      11,    0,  128,    2, 0x09 /* Protected */,
+      12,    0,  129,    2, 0x09 /* Protected */,
+      13,    0,  130,    2, 0x09 /* Protected */,
+      14,    0,  131,    2, 0x09 /* Protected */,
+      15,    0,  132,    2, 0x09 /* Protected */,
+      16,    1,  133,    2, 0x09 /* Protected */,
+      18,    0,  136,    2, 0x09 /* Protected */,
+      19,    0,  137,    2, 0x09 /* Protected */,
+      20,    3,  138,    2, 0x09 /* Protected */,
+      26,    2,  145,    2, 0x09 /* Protected */,
+      28,    1,  150,    2, 0x09 /* Protected */,
+      30,    1,  153,    2, 0x09 /* Protected */,
 
  // signals: parameters
     QMetaType::Void,
@@ -131,13 +133,14 @@ static const uint qt_meta_data_QPF__MainWindow[] = {
     QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
-    QMetaType::Void, QMetaType::QPoint,   16,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::QPoint,   17,
     QMetaType::Void,
     QMetaType::Void,
-    QMetaType::Void, QMetaType::QString, 0x80000000 | 21, 0x80000000 | 23,   20,   22,   24,
-    QMetaType::Void, 0x80000000 | 21, 0x80000000 | 26,   22,   24,
-    QMetaType::Void, QMetaType::Bool,   28,
-    QMetaType::Void, QMetaType::Int,   30,
+    QMetaType::Void, QMetaType::QString, 0x80000000 | 22, 0x80000000 | 24,   21,   23,   25,
+    QMetaType::Void, 0x80000000 | 22, 0x80000000 | 27,   23,   25,
+    QMetaType::Void, QMetaType::Bool,   29,
+    QMetaType::Void, QMetaType::Int,   31,
 
        0        // eod
 };
@@ -149,30 +152,31 @@ void QPF::MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int 
         switch (_id) {
         case 0: _t->goToOperational(); break;
         case 1: _t->commandSystem(); break;
-        case 2: _t->transitToOperational(); break;
-        case 3: _t->sendInData(); break;
-        case 4: _t->prepareSendMultInData(); break;
-        case 5: _t->selectInDataParamsFile(); break;
-        case 6: _t->prepareSendInDataFromFile(); break;
-        case 7: _t->sendMultInData(); break;
-        case 8: _t->sendInDataFromFile(); break;
-        case 9: _t->stopSendingMultInData(); break;
-        case 10: _t->handleFinishedHMI(); break;
-        case 11: _t->checkForTaskRes(); break;
-        case 12: _t->showTaskRes(); break;
-        case 13: _t->showTaskMonitContextMenu((*reinterpret_cast< const QPoint(*)>(_a[1]))); break;
-        case 14: _t->showWorkDir(); break;
-        case 15: _t->displayTaskInfo(); break;
-        case 16: _t->dumpTaskInfoToTree((*reinterpret_cast< QString(*)>(_a[1])),(*reinterpret_cast< Json::Value(*)>(_a[2])),(*reinterpret_cast< QTreeWidget*(*)>(_a[3]))); break;
-        case 17: _t->dumpToTree((*reinterpret_cast< Json::Value(*)>(_a[1])),(*reinterpret_cast< QTreeWidgetItem*(*)>(_a[2]))); break;
-        case 18: _t->setDebugInfo((*reinterpret_cast< bool(*)>(_a[1]))); break;
-        case 19: _t->sortTaskViewByColumn((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 2: _t->processPendingEvents(); break;
+        case 3: _t->transitToOperational(); break;
+        case 4: _t->sendInData(); break;
+        case 5: _t->prepareSendMultInData(); break;
+        case 6: _t->selectInDataParamsFile(); break;
+        case 7: _t->prepareSendInDataFromFile(); break;
+        case 8: _t->sendMultInData(); break;
+        case 9: _t->sendInDataFromFile(); break;
+        case 10: _t->stopSendingMultInData(); break;
+        case 11: _t->handleFinishedHMI(); break;
+        case 12: _t->checkForTaskRes(); break;
+        case 13: _t->showTaskRes(); break;
+        case 14: _t->showTaskMonitContextMenu((*reinterpret_cast< const QPoint(*)>(_a[1]))); break;
+        case 15: _t->showWorkDir(); break;
+        case 16: _t->displayTaskInfo(); break;
+        case 17: _t->dumpTaskInfoToTree((*reinterpret_cast< QString(*)>(_a[1])),(*reinterpret_cast< Json::Value(*)>(_a[2])),(*reinterpret_cast< QTreeWidget*(*)>(_a[3]))); break;
+        case 18: _t->dumpToTree((*reinterpret_cast< Json::Value(*)>(_a[1])),(*reinterpret_cast< QTreeWidgetItem*(*)>(_a[2]))); break;
+        case 19: _t->setDebugInfo((*reinterpret_cast< bool(*)>(_a[1]))); break;
+        case 20: _t->sortTaskViewByColumn((*reinterpret_cast< int(*)>(_a[1]))); break;
         default: ;
         }
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
         switch (_id) {
         default: *reinterpret_cast<int*>(_a[0]) = -1; break;
-        case 16:
+        case 17:
             switch (*reinterpret_cast<int*>(_a[1])) {
             default: *reinterpret_cast<int*>(_a[0]) = -1; break;
             case 2:
@@ -217,13 +221,13 @@ int QPF::MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 20)
+        if (_id < 21)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 20;
+        _id -= 21;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 20)
+        if (_id < 21)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 20;
+        _id -= 21;
     }
     return _id;
 }
