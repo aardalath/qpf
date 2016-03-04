3383a4d9916bab6aff95370316e182d2b78d2823
jgonza04 <jgonza04@463500fe-d6be-4184-9b20-47717e75ecac>
2016-03-04 10:37:10 +0000

    New DBG() macro for debugging info
    git-svn-id: https://euclid.esac.esa.int/svn/ESA/SOC/SOC-3-DEV/SOC-3-07-QLook/QPF/trunk@12024 463500fe-d6be-4184-9b20-47717e75ecac

---
+++ src/dbg.h
@@ -0,0 +1,88 @@
+/******************************************************************************
+ * File:    dbg.h
+ *          This file is part of QLA Processing Framework
+ *
+ * Domain:  QPF.libQPF.dbg
+ *
+ * Version: 1.0
+ *
+ * Date:    2015/07/01
+ *
+ * Copyright (C) 2015 J C Gonzalez
+ *_____________________________________________________________________________
+ *
+ * Topic: General Information
+ *
+ * Purpose:
+ *   Just some macros for debugging
+ *
+ * Created by:
+ *   J C Gonzalez
+ *
+ * Status:
+ *   Prototype
+ *
+ * Dependencies:
+ *   none
+ *
+ * Files read / modified:
+ *   none
+ *
+ * History:
+ *   See <Changelog>
+ *
+ * About: License Conditions
+ *   See <License>
+ *
+ ******************************************************************************/
+
+#ifndef DBG_H
+#define DBG_H
+
+//============================================================
+// Group: External Dependencies
+//============================================================
+
+//------------------------------------------------------------
+// Topic: System headers
+//   - iostream
+//   - cerrno
+//------------------------------------------------------------
+
+//------------------------------------------------------------
+// Topic: External packages
+//   none
+//------------------------------------------------------------
+
+//------------------------------------------------------------
+// Topic: Project headers
+//   none
+//------------------------------------------------------------
+
+#include <iostream>
+#include <cerrno>
+
+//============================================================
+// Group: Public Declarations
+//============================================================
+
+//------------------------------------------------------------
+// Topic: Macros
+//   - DEBUG_BUILD - Conditional macro (see DBG(s))
+//   - DBG(s)      - Shows a debug message on StdErr
+//------------------------------------------------------------
+
+#define DEBUG_BUILD
+
+#ifdef DEBUG_BUILD
+#  define DUMPVAR(v) #v << v
+#  define DBG(s) std::cerr						\
+  << __FILE__ << ':' << __FUNCTION__ << ':' << __LINE__			\
+  << ": " << s << std::endl << std::flush
+#else
+#  define DUMPVAR(v)
+#  define DBG(s)
+#endif
+
+#endif // DBG_H
+
