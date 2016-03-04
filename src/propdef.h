e87d6e4889a5a0bd52baaa5adf05449ec6bd320f
jgonza04 <jgonza04@463500fe-d6be-4184-9b20-47717e75ecac>
2016-03-04 10:37:35 +0000

    Added PropertyRef macro
    git-svn-id: https://euclid.esac.esa.int/svn/ESA/SOC/SOC-3-DEV/SOC-3-07-QLook/QPF/trunk@12050 463500fe-d6be-4184-9b20-47717e75ecac

--- src/propdef.h
+++ src/propdef.h
@@ -39,11 +39,26 @@
 #ifndef PROPDEF_H
 #define PROPDEF_H
 
+//============================================================
+// Group: Public Declarations
+//============================================================
+
 //------------------------------------------------------------
-// Topic: Declarations
+// Topic: Constants
+//   SET_NAMED_PROPERTIES - If defined, named property macro
+//                          is defined instead of normal
+//                          property macro.
 //------------------------------------------------------------
 
 #define SET_NAMED_PROPERTIES
+
+//------------------------------------------------------------
+// Topic: Macros
+//   - Property(c,t,p,n) - Defines an internal variable with
+//                         a getter and named setter
+//   - PropertyRef(c,t,p,n) - Defines an internal variable with
+//                            a getter and named setter
+//------------------------------------------------------------
 
 #ifdef SET_NAMED_PROPERTIES
 #define Property(c,t,p,n)					\
@@ -52,6 +67,11 @@
   inline t get ## n (void) const { return p; }			\
   inline t get ## n (void) { return p; }			\
   inline c & set ## n (t new ## n ) { p = new ## n ; return *this; }	
+#define PropertyRef(c,t,p,n)					\
+  private: t p;							\
+  public:							\
+  inline t & get ## n (void) { return p; }			\
+  inline c & set ## n (t & new ## n ) { p = new ## n ; return *this; }
 # define with(s)   s
 #else
 # define Property(t,p,n)					\
