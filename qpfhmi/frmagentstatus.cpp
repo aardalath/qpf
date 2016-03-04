f29cdec44f1318a2747307fc158c31cf939d0c64
jgonza04 <jgonza04@463500fe-d6be-4184-9b20-47717e75ecac>
2016-03-04 10:36:52 +0000

    Change to show separatedly the number of taks per status
    git-svn-id: https://euclid.esac.esa.int/svn/ESA/SOC/SOC-3-DEV/SOC-3-07-QLook/QPF/trunk@12005 463500fe-d6be-4184-9b20-47717e75ecac

--- qpfhmi/frmagentstatus.cpp
+++ qpfhmi/frmagentstatus.cpp
@@ -19,10 +19,14 @@ void FrmAgentStatus::updateInfo(TaskAgentInfo & d)
 {
     ui->lblTaskAgentName->setText(QString::fromStdString(d.name));
     ui->lblTotalTasks->setText(QString("%1").arg(d.total));
-    ui->lblRuningTasks->setText(QString("%1/%2/%3/%4/%5/%6")
-                                .arg(d.running).arg(d.waiting)
-                                .arg(d.paused).arg(d.stopped)
-                                .arg(d.failed).arg(d.finished));
+
+    ui->lblRun->setText(QString("%1").arg(d.running));
+    ui->lblWait->setText(QString("%1").arg(d.waiting));
+    ui->lblPau->setText(QString("%1").arg(d.paused));
+    ui->lblSto->setText(QString("%1").arg(d.stopped));
+    ui->lblFail->setText(QString("%1").arg(d.failed));
+    ui->lblFin->setText(QString("%1").arg(d.finished));
+
     ui->lblMaxTasks->setText(QString("%1").arg(d.maxnum));
     ui->lblClient->setText(QString::fromStdString(d.client));
     ui->lblServer->setText(QString::fromStdString(d.server));
@@ -35,11 +39,11 @@ void FrmAgentStatus::updateInfo(TaskAgentInfo & d)
 
     if (d.total > 0) {
         QHBoxLayout * hb = qobject_cast<QHBoxLayout*>(ui->frmStatusBar->layout());
-        hb->setStretch(0, d.running * 1000);
-        hb->setStretch(1, d.waiting * 1000);
-        hb->setStretch(2, d.paused * 1000);
-        hb->setStretch(3, d.stopped * 1000);
-        hb->setStretch(4, d.failed * 1000);
+        hb->setStretch(0, d.running  * 1000);
+        hb->setStretch(1, d.waiting  * 1000);
+        hb->setStretch(2, d.paused   * 1000);
+        hb->setStretch(3, d.stopped  * 1000);
+        hb->setStretch(4, d.failed   * 1000);
         hb->setStretch(5, d.finished * 1000);
         ui->frmStatusBar->setVisible(true);
     } else {
