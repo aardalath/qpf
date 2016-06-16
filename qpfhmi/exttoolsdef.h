92edbb7b0474ea5a339212608e0e8b397d023716
jgonza04 <jgonza04@463500fe-d6be-4184-9b20-47717e75ecac>
2016-06-16 06:46:18 +0000

    New/modified dialog/widget classes for definition of user def. tools
    git-svn-id: https://euclid.esac.esa.int/svn/ESA/SOC/SOC-3-DEV/SOC-3-07-QLook/QPF/branches/dev.HMIalone@14070 463500fe-d6be-4184-9b20-47717e75ecac

--- qpfhmi/exttoolsdef.h
+++ qpfhmi/exttoolsdef.h
@@ -2,10 +2,16 @@
 #define EXTTOOLSDEF_H
 
 #include <QDialog>
+#include <QModelIndex>
+#include <QTableWidgetItem>
+
+#include "exttooledit.h"
 
 namespace Ui {
 class ExtToolsDef;
 }
+
+namespace QPF {
 
 class ExtToolsDef : public QDialog
 {
@@ -15,8 +21,25 @@ public:
     explicit ExtToolsDef(QWidget *parent = 0);
     ~ExtToolsDef();
 
+    void initialize(MapOfUserDefTools & userTools, QStringList pts);
+    void getTools(MapOfUserDefTools & userTools);
+
+private slots:
+    void addNewTool();
+    void editTool(QModelIndex idx);
+    void editTool();
+    void editTool(int row);
+    void removeTool();
+    void cancelDlg();
+    void changeToolWithItem(QTableWidgetItem * item);
+
 private:
     Ui::ExtToolsDef *ui;
+    MapOfUserDefTools userDefTools;
+    MapOfUserDefTools origDefTools;
+    QStringList       prodTypes;
 };
+
+}
 
 #endif // EXTTOOLSDEF_H
