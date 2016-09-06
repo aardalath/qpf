46af2fa244c419ac60ae44132a717ba963e25020
jgonza04 <jgonza04@463500fe-d6be-4184-9b20-47717e75ecac>
2016-09-06 06:44:55 +0000

    New PostgreSQL Server Start script
    git-svn-id: https://euclid.esac.esa.int/svn/ESA/SOC/SOC-3-DEV/SOC-3-07-QLook/QPF/trunk@15032 463500fe-d6be-4184-9b20-47717e75ecac

---
+++ scripts/pgsql_start_server.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+##############################################################################
+# File       : pgsql_start_server.sh - PostgreSQL Server start
+# Domain     : QPF.scripts
+# Version    : 1.0
+# Date       : 2016/09/01
+# Copyright (C) 2015, 2016 J C Gonzalez
+#_____________________________________________________________________________
+# Purpose    : PostgreSQL Server start
+# Created by : J C Gonzalez
+# Status     : Prototype
+# History    : See <Changelog>
+###############################################################################
+
+pg_ctl -D /home/eucops/opt/pgsql/data -l /home/eucops/opt/pgsql/data/log start
+
