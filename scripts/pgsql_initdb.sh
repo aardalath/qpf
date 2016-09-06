52d40c32096ce934f25bb29e40f30df1804cf6e5
jgonza04 <jgonza04@463500fe-d6be-4184-9b20-47717e75ecac>
2016-09-06 06:44:39 +0000

    New PostgreSQL Database initialization script
    git-svn-id: https://euclid.esac.esa.int/svn/ESA/SOC/SOC-3-DEV/SOC-3-07-QLook/QPF/trunk@15031 463500fe-d6be-4184-9b20-47717e75ecac

---
+++ scripts/pgsql_initdb.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+##############################################################################
+# File       : pgsql_initdb.sh - PostgreSQL Database initialisation
+# Domain     : QPF.scripts
+# Version    : 1.0
+# Date       : 2016/09/01
+# Copyright (C) 2015, 2016 J C Gonzalez
+#_____________________________________________________________________________
+# Purpose    : PostgreSQL Database initialisation
+# Created by : J C Gonzalez
+# Status     : Prototype
+# History    : See <Changelog>
+###############################################################################
+
+rm -rf /home/eucops/opt/pgsql/data 
+mkdir -p  /home/eucops/opt/pgsql/data
+initdb -D /home/eucops/opt/pgsql/data
+
