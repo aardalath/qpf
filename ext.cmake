ac9edffa29e2264efce9247fb25a0716b0280051
jgonza04 <jgonza04@463500fe-d6be-4184-9b20-47717e75ecac>
2016-09-22 09:51:30 +0000

    First attemp to compile and link system with CMake
    git-svn-id: https://euclid.esac.esa.int/svn/ESA/SOC/SOC-3-DEV/SOC-3-07-QLook/QPF/trunk@15306 463500fe-d6be-4184-9b20-47717e75ecac

---
+++ ext.cmake
@@ -0,0 +1,71 @@
+#======================================================================
+# CMakeLists.txt
+# QPF - Prototype of QLA Processing Framework
+# General Project File
+#======================================================================
+# Author: J C Gonzalez - 2015, 2016
+# Copyright (C) 2015, 2016 Euclid SOC Team at ESAC
+#======================================================================
+
+#===== Projec dir. =======
+set (PRJ_ROOT_DIR $ENV{PWD})
+
+set (BUILD_ROOT build)
+
+#===== External libraries =======
+set (COTSDIR /opt/cots)
+set (HOME $ENV{HOME})
+
+message (STATUS "Now with ${PRJ_ROOT_DIR} . . .")
+
+if(EXISTS ${HOME}/opt)
+  message (STATUS "Using /opt")
+  set (ZMQDIR ${HOME}/opt/zmq)
+  set (CURLDIR ${HOME}/opt/curl)
+  set (PSQLDIR ${HOME}/opt/pgsql)
+  set (PCRE2DIR ${HOME}/opt/pcre2)
+elseif (EXISTS /opt)
+  message (STATUS "Using ${HOME}/opt")
+  set (ZMQDIR ${COTSDIR})
+  set (CURLDIR ${COTSDIR})
+  set (PSQLDIR /opt/pgsql)
+  set (PCRE2DIR ${COTSDIR})
+endif()
+
+# if (EXISTS ${HOME}/opt)
+  # set (GTESTDIR ${HOME}/ws/jcgg/cots/gtest-1.7.0)
+  # set (GTESTDIR ${HOME}/ws/cots/gtest-1.7.0)
+# endif()
+
+set (PSQLLIB pq)
+
+#===== Project sections/libraries =======
+set (LIBCOMMPATH ${PRJ}_ROOT_DIR)
+set (LIBCOMMLIB ${LIBCOMMPATH}/${BUILD}_ROOT/libcomm)
+set (LIBCOMMINC ${LIBCOMMPATH}/libcomm)
+
+set (JSONCPPPATH ${PRJ}_ROOT_DIR)
+set (JSONCPPLIB ${JSONCPPPATH}/${BUILD}_ROOT/json)
+set (JSONCPPINC ${JSONCPPPATH}/json)
+
+set (INFIXPATH ${PRJ}_ROOT_DIR)
+set (INFIXLIB ${INFIXPATH}/${BUILD}_ROOT/infix)
+set (INFIXINC ${INFIXPATH}/infix)
+
+set (SDCPATH ${PRJ}_ROOT_DIR)
+set (SDCLIB ${SDCPATH}/${BUILD}_ROOT/sdc)
+set (SDCINC ${SDCPATH}/sdc)
+
+#===== Compilation/Linkage variables =====
+# QMAKE_CXXFLAGS += -g3 -std=c++11 -Wall -Wextra -D_GLIBCXX_USE_CXX11_ABI=0
+# LIBS += -lpcre2-8
+#
+# INCLUDEPATH += . ${PRJ}_ROOT_DIR/src
+# INCLUDEPATH += ${LIBCOMMINC} ${JSONCPPINC} ${INFIXINC} ${SDCINC}
+# INCLUDEPATH += ${ZMQDIR}/include ${CURLDIR}/include ${PCRE}2DIR/include ${PSQLDIR}/include
+# INCLUDEPATH += ${HOME}/opt
+#
+# OLD_LIBDIR = ${QMAKE}_LIBDIR
+# QMAKE_LIBDIR += ${LIBCOMMLIB} ${JSONCPPLIB} ${INFIXLIB} ${SDCLIB}
+# QMAKE_LIBDIR += ${ZMQDIR}/lib ${CURLDIR}/lib ${PCRE}2DIR/lib ${PSQLDIR}/lib
+# QMAKE_LIBDIR += /usr/lib64 /lib64 ${OLD}_LIBDIR
