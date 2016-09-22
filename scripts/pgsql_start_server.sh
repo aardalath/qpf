cd988401f444d34e27b7f940b2e17de7eeb27a1d
jgonza04 <jgonza04@463500fe-d6be-4184-9b20-47717e75ecac>
2016-09-22 09:46:37 +0000

    Error checking for server start
    git-svn-id: https://euclid.esac.esa.int/svn/ESA/SOC/SOC-3-DEV/SOC-3-07-QLook/QPF/trunk@15305 463500fe-d6be-4184-9b20-47717e75ecac

--- scripts/pgsql_start_server.sh
+++ scripts/pgsql_start_server.sh
@@ -12,7 +12,20 @@
 # History    : See <Changelog>
 ###############################################################################
 
+# Prepare internal folder
 sudo chmod 777 /var/run/postgresql
+sudo chown -R postgres:postgres /var/run/postgresql
+
+# Try to start server
 pg_ctl -D /home/eucops/opt/pgsql/data -l /home/eucops/opt/pgsql/data/log start
 
- 
+# Check server status
+status=$(pg_ctl -D /home/eucops/opt/pgsql/data status 2>&1 | head -1 | cut -d" " -f 4)
+if [ "$status" == "running" ]; then
+    echo "Server is now running"
+    exit 0
+else
+    echo "ERROR: Server could not be started."
+    exit 1
+fi
+
