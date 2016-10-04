f0ec1c6f8c87383d90239a5ff5f266b808430f20
jgonza04 <jgonza04@463500fe-d6be-4184-9b20-47717e75ecac>
2016-10-04 09:06:24 +0000

    New version file for QPF core
    git-svn-id: https://euclid.esac.esa.int/svn/ESA/SOC/SOC-3-DEV/SOC-3-07-QLook/QPF/trunk@15446 463500fe-d6be-4184-9b20-47717e75ecac

---
+++ qpf/version.h
@@ -0,0 +1,80 @@
+/******************************************************************************
+ * File:    version.h
+ *          This file is part of QLA Processing Framework
+ *
+ * Domain:  QPF.qpf.version
+ *
+ * Version: 1.0
+ *
+ * Date:    2015/07/01
+ *
+ * Copyright (C) 2015 J C Gonzalez
+ *_____________________________________________________________________________
+ *
+ * Topic: General Information
+ *
+ * Purpose:
+ *   Version constants
+ *
+ * Created by:
+ *   J C Gonzalez
+ *
+ * Status:
+ *   Prototype
+ *
+ * Dependencies:
+ *   none
+ *
+ * Files read / modified:
+ *   none
+ *
+ * History:
+ *   See <Changelog>
+ *
+ * About: License Conditions
+ *   See <License>
+ *
+ ******************************************************************************/
+
+//! \file version.h
+
+#ifndef VERSION_H
+#define VERSION_H
+
+//! Application System Name
+#define APP_SYS_NAME     "QPF"
+
+//! Application name
+#define APP_NAME         "QPF"
+
+//! Application release number
+#define APP_RELEASE      "1.0.1"
+
+//! Application release date
+#define APP_DATE         "OCT 2016"
+
+//! Application developer company
+#define APP_COMPANY      "Euclid SOC Team"
+
+//! Application developer company
+#define APP_DOMAIN       "jcg"
+
+//! application developer division
+#define APP_DIVISION     ""
+
+//! Application long name
+#define APP_LONG_NAME    "QLA Processing Framework"
+
+//! Application short purpose description
+#define APP_PURPOSE      "Euclid QLA Processing Framework"
+
+//! Application short purpose description
+#define APP_TITLE        "QPF"
+
+//! Developer Company
+#define APP_CREATED_BY   APP_COMPANY
+
+//! Copyright Notice
+#define APP_COPYRIGHT    "(C) Euclid SOC Team"
+
+#endif /* VERSION_H */
