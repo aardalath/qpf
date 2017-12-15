bf9552583576fb797ad3313e4996d4c4b52aa15a
J C Gonzalez <JCGonzalez@sciops.esa.int>
2017-12-15 14:52:49 +0100

    Initial (empty) code for classes AnalysisSession, ExportHandler and VOSpaceHandler

---
+++ qpfhmi/anasession.cpp
@@ -0,0 +1,54 @@
+/******************************************************************************
+ * File:    anasession.cpp
+ *          This file is part of QLA Processing Framework
+ *
+ * Domain:  QPF.libQPF.anasession
+ *
+ * Version:  1.2
+ *
+ * Date:    2015/07/01
+ *
+ * Author:   J C Gonzalez
+ *
+ * Copyright (C) 2015-2017 Euclid SOC Team @ ESAC
+ *_____________________________________________________________________________
+ *
+ * Topic: General Information
+ *
+ * Purpose:
+ *   Provides object implementation for some declarations
+ *
+ * Created by:
+ *   J C Gonzalez
+ *
+ * Status:
+ *   Prototype
+ *
+ * Dependencies:
+ *   none
+ *
+ * Files read / modified:
+ *   none
+ *
+ * History:
+ *   See <Changelog>
+ *
+ * About: License Conditions
+ *   See <License>
+ *
+ ******************************************************************************/
+
+#include "anasession.h"
+
+namespace QPF {
+
+AnalysisSession::AnalysisSession(QObject *parent)
+{
+}
+
+AnalysisSession::~AnalysisSession()
+{
+}
+
+
+}
