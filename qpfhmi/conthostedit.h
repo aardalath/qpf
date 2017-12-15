e52794062b08eb908dc28b0e14dcf85870a47d8d
J C Gonzalez <JCGonzalez@sciops.esa.int>
2017-12-15 14:52:50 +0100

    New dialog class to edit Container host definitions from ConfigTool

---
+++ qpfhmi/conthostedit.h
@@ -0,0 +1,72 @@
+/******************************************************************************
+ * File:    conthostedit.h
+ *          Declaration of class ContHostEdit
+ *
+ * Domain:  QPF.qpfgui.conthostedit
+ *
+ * Version:  1.2
+ *
+ * Date:    2016-11-03
+ *
+ * Author:   J C Gonzalez
+ *
+ * Copyright (C) 2015-2017 Euclid SOC Team @ ESAC
+ *_____________________________________________________________________________
+ *
+ * Topic: General Information
+ *
+ * Purpose:
+ *   Declaration of class ContHost
+ *
+ * Created by:
+ *   J C Gonzalez
+ *
+ * Status:
+ *   Prototype
+ *
+ * Dependencies:
+ *   none
+ *
+ * Files read / modified:
+ *   none
+ *
+ * History:
+ *   See <ChangeLog>
+ *
+ * About: License Conditions
+ *   See <License>
+ *
+ ******************************************************************************/
+
+#ifndef CONTHOSTEDIT_H
+#define CONTHOSTEDIT_H
+
+#include <QDialog>
+
+#include "hmitypes.h"
+
+namespace Ui {
+class ContHostEdit;
+}
+
+namespace QPF {
+
+class ContHostEdit : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit ContHostEdit(QWidget *parent = 0);
+    ~ContHostEdit();
+
+public:
+    void setContHost(QString ip, bool isMaster, int numAg);
+    void getContHost(QString & ip, bool & isMaster, int & numAg);
+
+private:
+    Ui::ContHostEdit *ui;
+};
+
+}
+
+#endif // CONTHOSTEDIT_H
