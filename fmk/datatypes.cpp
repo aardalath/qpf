1cd20982cfa082ee921cc360fc00d737e2eee333
J C Gonzalez <josec.glez@gmail.com>
2018-02-16 15:56:52 +0100

    Added sortedStr() to JValue class

--- fmk/datatypes.cpp
+++ fmk/datatypes.cpp
@@ -58,4 +58,82 @@ std::map<TaskStatus, std::string> TaskStatusName = { TLIST_TASK_STATUS };
 std::map<std::string, TaskStatus> TaskStatusValue = { TLIST_TASK_STATUS };
 #undef T
 
+JValue::JValue()
+{
+}
+
+JValue::JValue(json val)
+{
+    value = val; 
+}
+
+JValue::JValue(std::string s)
+{
+    fromStr(s); 
+}
+
+JValue JValue::operator()(std::string key)
+{
+    return JValue(value[key]); 
+}
+
+json & JValue::operator[](std::string key)
+{
+    return value[key]; 
+}
+
+std::string JValue::str(bool styled)
+{
+    if (value.isObject() || value.isArray()) {
+        if (! styled) {
+            Json::FastWriter w;
+            return w.write(value);
+        } else {
+            Json::StyledWriter w;
+            return w.write(value);
+        }
+    } else {
+        return value.asString();
+    }
+}
+
+std::string JValue::sortedStr(std::vector<std::string> sortKeys)
+{
+    json sortedValue;
+    char newKey[100];
+    int i = 0;
+    for (auto & key : sortKeys) {
+        sprintf(newKey, "SORTEDKEY:%03d-%s", i++, key.c_str());
+        sortedValue[newKey] = value[key];
+    }
+    Json::StyledWriter w;
+    std::string result = w.write(sortedValue);
+    std::size_t pos = 0;       
+    while ((pos = result.find("SORTEDKEY:", pos)) != std::string::npos) {
+        result.erase(pos, 14);
+    }
+    return result;
+}
+
+void JValue::fromStr(std::string content)
+{
+    Json::Reader reader;
+    reader.parse(content, value);
+}
+
+json & JValue::val()
+{
+    return value; 
+}
+
+int JValue::size()
+{
+    return value.size(); 
+}
+
+bool JValue::has(const char * key)
+{
+    return value.isMember(key); 
+}
+
 //}
